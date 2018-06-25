1c9f61a01c03aee41263ef4b7ae561da3a2a5be4
Brajesh Singh <35314485+singhbrajesh4142@users.noreply.github.com>
2018-06-26 00:42:42 +0530

    Update head.php

--- head.php
+++ head.php
@@ -8,7 +8,7 @@ session_start()?>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
-    <title>Ashish</title>
+    <title>Blog</title>
 
     <!-- Bootstrap -->
     <link href="Bootstrap/css/bootstrap.min.css" rel="stylesheet">
